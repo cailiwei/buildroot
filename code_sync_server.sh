1b439c51880da19e15421a37ef8ec1c11ce99da9
Levy <cailiwei712@163.com>
2013-07-30 22:27:47 +0800

    Modified the code_sync script secondly

--- code_sync_server.sh
+++ code_sync_server.sh
@@ -19,10 +19,30 @@ fi
 if [ "$proxy" = "http" ]; then
 	git remote set-url origin https://github.com/cailiwei/buildroot.git
 	git remote show origin
+	git push origin master:next
+	git pull --rebase
+	var=`git branch | grep next`
+	if [ "$var" = "next" ]; then
+		git pull --rebase
+	else
+		git checkout -b next origin/next
+	fi
+	git checkout master
+	git merge next
 	git push origin master
 else
 	git remote set-url origin git@github.com:cailiwei/buildroot.git
 	git remote show origin
+	git pull --rebase
+	git push origin master:next
+	var=`git branch | grep next`
+	if [ "$var" = "next" ]; then
+		git pull --rebase
+	else
+		git checkout -b next origin/next
+	fi
+	git checkout master
+	git merge next
 	git push origin master
 fi
 # ----------------------------------------------------------
