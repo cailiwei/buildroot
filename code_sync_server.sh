05acd8b0d7aff5e3bf49eb4f0feee4e1ab35e1cf
Levy <cailiwei712@163.com>
2013-07-30 21:32:26 +0800

    Modified the code_sync script

--- code_sync_server.sh
+++ code_sync_server.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 # set server-url: "http://git.buildroot.net/git/buildroot.git" or "git://git.buildroot.net/buildroot"
-git remote set-url origin http://git.buildroot.net/git/buildroot.git
-git remote show origin
-git fetch
-git pull --rebase
+proxy=$1
+
+if [ "$proxy" = "http" ]; then
+	git remote set-url origin http://git.buildroot.net/git/buildroot.git
+	git remote show origin
+	git fetch
+	git pull --rebase
+else
+	git remote set-url origin git://git.buildroot.net/buildroot
+	git remote show origin
+	git fetch
+	git pull --rebase
+fi
+
 # ----------------------------------------------------------
 # set server-url: "https://github.com/cailiwei/buildroot.git" or "git@github.com:cailiwei/buildroot.git"
-git remote set-url origin https://github.com/cailiwei/buildroot.git
-git remote show origin
-git push origin master
+if [ "$proxy" = "http" ]; then
+	git remote set-url origin https://github.com/cailiwei/buildroot.git
+	git remote show origin
+	git push origin master
+else
+	git remote set-url origin git@github.com:cailiwei/buildroot.git
+	git remote show origin
+	git push origin master
+fi
 # ----------------------------------------------------------
 
 echo "Code sync process end !!!"
