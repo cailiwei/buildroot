94f8234a58fff2ad4997ba58637b0225197c0541
Levy <cailiwei712@163.com>
2013-07-30 23:06:26 +0800

    Thirdly modified the script

--- code_sync_server.sh
+++ code_sync_server.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 # set server-url: "http://git.buildroot.net/git/buildroot.git" or "git://git.buildroot.net/buildroot"
+echo "############### Begin sync local code to remote origin #########################"
 proxy=$1
 
 if [ "$proxy" = "http" ]; then
 	git remote set-url origin http://git.buildroot.net/git/buildroot.git
-	git remote show origin
 	git fetch
 	git pull --rebase
 else
 	git remote set-url origin git://git.buildroot.net/buildroot
-	git remote show origin
 	git fetch
 	git pull --rebase
 fi
+# Look for the remote origin
+echo "##################################################################################"
+git remote show origin
+echo "##################################################################################"
 
 # ----------------------------------------------------------
 # set server-url: "https://github.com/cailiwei/buildroot.git" or "git@github.com:cailiwei/buildroot.git"
 if [ "$proxy" = "http" ]; then
 	git remote set-url origin https://github.com/cailiwei/buildroot.git
-	git remote show origin
 	git push origin master:next
 	git pull --rebase
 	var=`git branch | grep next`
@@ -32,9 +34,8 @@ if [ "$proxy" = "http" ]; then
 	git push origin master
 else
 	git remote set-url origin git@github.com:cailiwei/buildroot.git
-	git remote show origin
-	git pull --rebase
 	git push origin master:next
+	git pull --rebase
 	var=`git branch | grep next`
 	if [ "$var" = "next" ]; then
 		git pull --rebase
@@ -45,6 +46,10 @@ else
 	git merge next
 	git push origin master
 fi
+
+# Look for the remote origin info
+git remote show origin
 # ----------------------------------------------------------
 
+echo "################### End of sync ######################################################"
 echo "Code sync process end !!!"
